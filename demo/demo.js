8000b309584706d9c0c8037a3c8f23c2a4c6e24c
Sven <sven.buschbeck@gmail.com>
2018-03-06 16:37:08 -0600

    simplified API

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,8 @@ import config from './config.js';
 import KeystoreClient from '../keystore-client.js';
 import Policy from '/libraries/keystore/policy.js';
 
+window.Policy = Policy;
+
 (async function() {
     console.log("Keystore client config: ", config);
 
@@ -9,7 +11,7 @@ import Policy from '/libraries/keystore/policy.js';
     console.log("Keystore client: ", keystoreApi);
 
     const limit = 1000;
-    const requiredPolicy = Policy.get("full", [ limit ]);
+    const requiredPolicy = Policy.get("spending-limit", limit);
     console.log("requiredPolicy", requiredPolicy);
 	const grantedPolicy = await keystoreApi.getPolicy();
     console.log("grantedPolicy", grantedPolicy);
@@ -17,7 +19,7 @@ import Policy from '/libraries/keystore/policy.js';
 	if (!requiredPolicy.equals(grantedPolicy)) {
     	const authorizedPolicy = await keystoreApi.authorize(requiredPolicy);
         console.log("authorizedPolicy", authorizedPolicy);
-        
+
     	if (!requiredPolicy.equals(authorizedPolicy))
             throw { message: "KeystoreClient: Policies don't match", policies: [requiredPolicy, grantedPolicy, authorizedPolicy] }
     }
