ad34cce16dd598b04a54d8442c5a09944a6939ca
sisou <hello@soerenschwert.de>
2018-03-08 23:44:11 +0100

    Adapt config file style

--- demo/config.js
+++ demo/config.js
@@ -1,13 +1,13 @@
-const localConfig = {
-    keyguardSrc: `${location.origin}:8080/libraries/keyguard/keyguard.html`
+const devConfig = {
+    keyguardSrc: `${location.origin}/libraries/keyguard/keyguard.html`
 }
 
-const alternativeConfig = {
-    keyguardSrc: `${location.origin}/libraries/keyguard/keyguard.html`
+const localConfig = {
+
 }
 
 const liveConfig = {
     keyguardSrc: 'https://secure.nimiq.com/keyguard.html'
 }
 
-export default alternativeConfig;
+export default devConfig;
