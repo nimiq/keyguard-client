15cbf39a1b5aea8ef7ced2394897ae43e5ee91e9
Don Maxito <maxharz@gmail.com>
2018-03-12 23:09:18 -0600

    extent demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,8 @@
 import config from './config.js';
 import KeyguardClient from '../keyguard-client.js';
 import WalletPolicy from '/libraries/keyguard/access-control/wallet-policy.js';
+import SafePolicy from '/libraries/keyguard/access-control/safe-policy.js';
+import * as AccountType from '/libraries/keyguard/accounts/account-type.js';
 
 request.style.display = "none";
 
@@ -14,7 +16,7 @@ async function ensureIsAuthorized(resolve, reject) {
     console.log("granted policy", grantedPolicy);
 
 	if (!grantedPolicy) {
-        const requiredPolicy = new WalletPolicy(100);
+        const requiredPolicy = new SafePolicy();
         console.log("Authorize required policy", requiredPolicy);
         request.style.display = "block";
         authorize.addEventListener('click', async e => {
@@ -32,6 +34,18 @@ async function ensureIsAuthorized(resolve, reject) {
 new Promise(ensureIsAuthorized).then(async (keyguardApi) => {
     console.log("Authorized! Continue...");
 
-    const accounts = await keyguardApi.getAccounts();
-    console.log(accounts);
+    let accounts = await keyguardApi.getAccounts();
+
+    console.log(`Accounts: ${JSON.stringify(accounts)}`);
+
+    const volatileAccounts = await keyguardApi.createVolatileAccounts(2);
+
+    console.log(`Volatile accounts: ${volatileAccounts}`);
+
+    await keyguardApi.persistAccount(volatileAccounts[0], AccountType.Low);
+
+    accounts = await keyguardApi.getAccounts();
+
+    console.log(`Accounts after persisting first volatile account: ${JSON.stringify(accounts)}`);
+
 })
